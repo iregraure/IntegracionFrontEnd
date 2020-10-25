0003e9b110a77c72158f51e2852d073d07918e20
iregraure <iregraure@gmail.com>
2020-10-25 21:04:35 +0100

    Comentado el código JavaScipt

--- videoRentalShop.js
+++ videoRentalShop.js
@@ -10,26 +10,38 @@ let divResultados = document.getElementById("resultado");
 
 // Funcionamiento del botón para mostrar la lista de los clientes
 botonListarClientes.addEventListener('click', (e) => {
+    // Evitamos el comportamiento por defecto
     e.preventDefault();
+    // Eliminamos la tabla con el listado de clientes
     eliminaTabla();
+    // Borramos los datos del formulario
     resetFormulario();
+    // Recuperamos los datos del servidor
     fetch('http://127.0.0.1:8080/api/customer')
         .then(res => res.ok ? Promise.resolve(res) : Promise.reject(res))
         .then(res => res.json())
         .then(res => {
+            // Creamos la tabla y le añadimos un id
             let tabla = document.createElement("table");
             tabla.id = "clientes";
-            let pos = 1;
+            // Añadimos la cabecera de la tabla
             aniadePrimeraFila(tabla);
+            // Creamos una variable en la que vamos a guardar la posición en la que hay que añadir la siguiente fila
+            let pos = 1;
+            // Recorremos los datos que hemos recuperado del servidor, los añadimos a la tabla e incrementamos la posición de la siguiente fila
             res.forEach(customer => {
                 aniadeFila(customer, pos, tabla);
                 pos++;
             });
+            // Cuando hemos terminado de crear la tabla la añadimos al HTML
             divResultados.appendChild(tabla);
         })
+        // Vamos a controlar los errores
         .catch(error => {
+            // Si nos devuelve un 404 es que no existen clientes
             if (error.status == 404) {
                 alert("No existen clientes");
+                // Cualquier otro error es inesperado
             } else {
                 alert("Se ha producido un error inesperado");
             }
@@ -38,42 +50,63 @@ botonListarClientes.addEventListener('click', (e) => {
 
 // Funcionamiento del botón para mostrar el cliente con el id que se le pase
 botonMostrarCliente.addEventListener('click', (e) => {
+    // Evitamos el comportamiento por defecto
     e.preventDefault();
+    // Eliminamos la tabla con el listado de clientes
     eliminaTabla();
+    // Obtenemos el id que ha introducido el usuario
     let id = document.getElementById("idGet").value;
+    // Si no se ha introducido ningún id mostramos un mensaje de error
     if (id == "") {
         alert("Debe introducir un id");
     } else {
+        // Si se ha introducido un id, recuperamos el cliente del servidor
         fetch(`http://127.0.0.1:8080/api/customer/${id}`)
             .then(res => res.ok ? Promise.resolve(res) : Promise.reject(res))
             .then(res => res.json())
             .then(res => {
+                // Mostramos los datos del cliente en sus correspondientes campos del formulario
                 document.getElementById("fullName").value = res.fullName;
                 document.getElementById("address").value = res.address;
                 document.getElementById("phoneNumber").value = res.phoneNumber;
                 document.getElementById("birthDate").value = res.birthDate;
                 document.getElementById("dni").value = res.dni;
+                // Borramos el id que ha introducido el usuario
                 document.getElementById("idGet").value = "";
             })
+            // Vamos a controlar los errores
             .catch(res => {
-                alert(`No existe ningún cliente con el id ${id}`);
-                document.getElementById("idGet").value = "";
+                // Si nos devuelve un 404 es que no existe ningún cliente con el id introducido
+                if (error.status == 404) {
+                    alert(`No existe ningún cliente con el id ${id}`);
+                    document.getElementById("idGet").value = "";
+                    // Cualquier otro error es inesperado
+                } else {
+                    alert("Se ha producido un error inesperado");
+                    document.getElementById("idGet").value = "";
+                }
             })
     }
 });
 
 // Comportamiento del botón para añadir un cliente
 botonAñadirCliente.addEventListener('click', (e) => {
+    // Evitamos el comportamiento por defecto
     e.preventDefault();
+    // Eliminamos la tabla con el listado de clientes
     eliminaTabla();
+    // Recuperamos el dni que ha introducido el usuario
     let dni = document.getElementById("dni").value;
+    // Si el dni está vacío mostramos un mensaje de error
     if (dni == "") {
         alert("El campo dni no puede estar vacío");
     } else {
+        // Si se ha introducido un dni, recuperamos el resto de campos del formulario
         let fullName = document.getElementById("fullName").value;
         let address = document.getElementById("address").value;
         let phoneNumber = document.getElementById("phoneNumber").value;
         let birthDate = document.getElementById("birthDate").value;
+        // Realizamos la petición POST al servidor
         fetch('http://127.0.0.1:8080/api/customer', {
                 method: 'POST',
                 body: JSON.stringify({
@@ -89,12 +122,16 @@ botonAñadirCliente.addEventListener('click', (e) => {
             })
             .then(res => res.ok ? Promise.resolve(res) : Promise.reject(res))
             .then(res => {
+                // Si el cliente se crea, mostramos un mensaje indicándolo
                 alert("El cliente se ha añadido de forma correcta");
                 resetFormulario();
             })
+            // Si no se puede crear el cliente
             .catch(error => {
+                // Si el error es el 409 quiere decir que ya existe un cliente con ese dni
                 if (error.status == 409) {
                     alert('Ya existe un cliente con ese dni');
+                    // Cualquier otro error es inesperado
                 } else {
                     alert('Se ha producido un error inesperado.');
                 }
@@ -104,33 +141,46 @@ botonAñadirCliente.addEventListener('click', (e) => {
 
 // Comportamiento del botón para borrar los datos del formulario
 botonResetFormulario.addEventListener('click', (e) => {
+    // Evitamos el comportamiento por defecto
     e.preventDefault();
+    // Llamamos a la función que va a borrar los datos del formulario
     resetFormulario();
 })
 
 // Comportamiento del botón para eliminar un cliente
 botonEliminarCliente.addEventListener('click', (e) => {
+    // Evitamos el comportamiento por defecto
     e.preventDefault();
+    // Eliminamos la tabla con el listado de clientes
     eliminaTabla();
+    // Eliminamos los datos del fromulario
     resetFormulario();
+    // Recuperamos el id que ha introducido el usuario
     let id = document.getElementById("idDelete").value;
+    // Si no se ha introducido ningún id mostraremos un mensaje de error
     if (id == "") {
         alert("Debe introducir un id");
     } else {
+        // Si se ha introducido un id, realizamos la petición DELETE al serrvidor
         fetch(`http://127.0.0.1:8080/api/customer/${id}`, {
                 method: 'DELETE'
             })
             .then(res => res.ok ? Promise.resolve(res) : Promise.reject(res))
+            // Si se elimina el cliente mostramos un mensaje y borramos el id introducido
             .then(res => {
                 alert("Se ha eliminado el cliente");
                 document.getElementById("idDelete").value = "";
             })
+            // Si no se ha podido eliminar el cliente
             .catch(error => {
+                // Si devuelve el error 404 es que no existe ningún cliente con ese id
                 if (error.status == 404) {
                     alert(`No existe ningún cliente con el id ${id}`);
                     document.getElementById("idDelete").value = "";
+                    // Cualquier otro error es inesperado
                 } else {
                     alert('Se ha producido un error inesperado.');
+                    document.getElementById("idDelete").value = "";
                 }
             })
     }
@@ -138,12 +188,18 @@ botonEliminarCliente.addEventListener('click', (e) => {
 
 // Comportamiento del botón para actualizar un cliente
 botonActualizarCliente.addEventListener('click', (e) => {
+    // Evitamos el comportamiento por defecto
     e.preventDefault();
+    // Eliminamos la tabla con los datos de todos los clientes
     eliminaTabla();
+    // Recuperamos el id que ha introducido el usuario
     let id = document.getElementById("idUpdate").value;
+    // Si no se introduce ningún id, mostramos un mensaje de error
     if (id == "") {
         alert("Debe introducir un id");
     } else {
+        // Comprobamos el valor del todos los campos. Si no se ha introducido nada almacenamos "null"
+        // No comprobamos el valor del dni porque no se permite que se modifique
         let fullName = document.getElementById("fullName").value;
         if (fullName == "") {
             fullName = null;
@@ -160,6 +216,7 @@ botonActualizarCliente.addEventListener('click', (e) => {
         if (birthDate == "") {
             birthDate = null;
         }
+        // Realizamos una petición PUT al servidor
         fetch(`http://127.0.0.1:8080/api/customer/${id}`, {
                 method: 'PUT',
                 body: JSON.stringify({
@@ -175,15 +232,20 @@ botonActualizarCliente.addEventListener('click', (e) => {
             })
             .then(res => res.ok ? Promise.resolve(res) : Promise.reject(res))
             .then(res => {
-                console.log(res)
+                // Si se actualiza el cliente mostramos un mensaje
                 alert("El cliente se ha actualizado de forma correcta");
+                // Borramos los datos del formulario
                 resetFormulario();
+                // Borramos el id que ha introducido el usuario
                 document.getElementById("idUpdate").value = "";
             })
+            // Si no se puede actualizar el cliente
             .catch(error => {
+                // Si devuelve el error 404 es que no existe ningún cliente con ese id
                 if (error.status == 404) {
                     alert(`No existe ningún cliente con el id ${id}`);
                     document.getElementById("idUpdate").value = "";
+                    // Cualquier otro error es inesperado
                 } else {
                     alert('Se ha producido un error inesperado.');
                 }
@@ -191,13 +253,17 @@ botonActualizarCliente.addEventListener('click', (e) => {
     }
 })
 
+// Función para eliminar la tabla con los datos de todos los clientes
 const eliminaTabla = () => {
+    // Recuperamos el elemento que tiene el id "clientes"
     let tabla = document.getElementById("clientes");
+    // Si la tabla existe, se elimina
     if (tabla != null) {
         tabla.parentNode.removeChild(tabla);
     }
 }
 
+// Función que añade la cabecera de la tabla
 const aniadePrimeraFila = (tabla) => {
     // Creamos las celdas de la cabecera y le damos su valor
     let id = document.createElement("th");
@@ -224,6 +290,8 @@ const aniadePrimeraFila = (tabla) => {
     tabla.appendChild(fila);
 }
 
+// Función que añade una fila de datos a la tabla
+// Hay que pasarle el objeto que vamos a añadir, la posición de la fila y la tabla
 const aniadeFila = (customer, pos, tabla) => {
     // Añadimos la fila a la tabla
     let fila = tabla.insertRow(pos);
@@ -243,7 +311,9 @@ const aniadeFila = (customer, pos, tabla) => {
     dni.innerHTML = customer.dni;
 }
 
+// Función que va a eliminar los campos del formulario
 const resetFormulario = () => {
+    // Para cada uno de los input del formulario le ponemos su valor a ""
     document.getElementById("fullName").value = "";
     document.getElementById("address").value = "";
     document.getElementById("phoneNumber").value = "";
